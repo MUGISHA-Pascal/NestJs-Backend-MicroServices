019f9c1e2894e1389ea3bee58e58e45f86a11f71
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-13 07:10:58 +0200

    adding the microservice setup in every file

--- inventory-service/src/main.ts
+++ inventory-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: Number(process.env.PORT),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as dotenv from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: Number(process.env.PORT),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
