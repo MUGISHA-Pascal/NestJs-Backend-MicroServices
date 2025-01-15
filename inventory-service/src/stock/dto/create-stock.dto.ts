9d346bfa9d743848674a9e0e3394d40806462de2
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-15 13:43:37 +0200

    new initialization

--- inventory-service/src/stock/dto/create-stock.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
-export class CreateStockDto {
-  @IsNumber()
-  @IsNotEmpty()
-  quantity: number;
-  @IsNumber()
-  @IsNotEmpty()
-  productId: number;
-}
